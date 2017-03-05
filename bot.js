d5a346365581b3311e56f6acea5744870f7ddfef
Alexandria Bell <bubbiepops000@gmail.com>
2017-03-05 14:37:23 +0000

    improvements?

--- bot.js
+++ bot.js
@@ -3,14 +3,10 @@ let Token=require("./bot.json").token;
 const Eris=require("eris");
 
 var bot=new Eris.Client(Token);
-var botuser;
 
-bot.on("ready",()=>
-{
-	botuser=bot.user;
-	console.log(`${botuser.username}#${botuser.discriminator} is ready for use.`);
+bot.on("ready",()=>{
+	console.log(`${bot.user.username}#${bot.user.discriminator} is ready for use.`);
 	bot.editStatus("online",{name:"with Alex"});
-}
-);
+});
 
 bot.connect();
