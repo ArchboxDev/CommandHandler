e3fae685062f914f21252db6e896ccb4fc5bf8b0
Alexandria Bell <bubbiepops000@gmail.com>
2017-03-05 17:01:51 +0000

    updates thanks to @Bubbie

--- bot.js
+++ bot.js
@@ -3,8 +3,17 @@ let Token=require("./bot.json").token;
 const Eris=require("eris"),
 	bot=new Eris.Client(Token)
 
+const CommandHandler=require("./commands/handler.js"),
+	Commands=new CommandHandler(bot,"a.")
+
+Commands.register("ping",(msg,args)=>{
+	console.log( "Ping" );
+	return `**Pong!** ${msg.channel.guild.shard.latency}ms`;
+});
+
 bot.on("ready",()=>{
-	console.log(`${bot.user.username}#${bot.user.discriminator} is ready for use.`);
+	console.log(`[Bot] ${bot.user.username}#${bot.user.discriminator} is ready for use.`);
+	
 	bot.editStatus("online",{name:"with Alex"});
 });
 
---
+++ commands/handler.js
@@ -0,0 +1,42 @@
+class Handler {
+	constructor(bot,prefix){
+		this.bot=bot;
+		this.prefix=prefix;
+		
+		this.commands={};
+		
+		bot.on("messageCreate",(msg)=>{
+			if (!msg.cleanContent.startsWith(this.prefix)) return;
+			var args = msg.cleanContent.split(" ");
+			
+			for (let cmd in this.commands) {
+				let payload=this.commands[cmd];
+				if (typeof(payload)!="function")
+					throw new Error(`Command payload for "${cmd}" isn't a function.`);
+				
+				if (args[0]==this.prefix+cmd){
+					let reply=payload(msg,args);
+					if (reply!=undefined&&reply!=null)
+						bot.createMessage(msg.channel.id,reply);
+				}
+			}
+		});
+	}
+	
+	register(name,payload){
+		if (!name) throw new Error("No name for command.");
+		if (!payload) throw new Error(`No payload for command "${name}".`);
+		if (typeof(payload)!="function") throw new Error(`Command payload for "${name}" isn't a function.`);
+		
+		return this.commands[name]=payload;
+	}
+	
+	unregister(name){
+		if (!name) throw new Error("No name for command.");
+		if (!this.commands[name]) throw new Error(`Command "${name}" doesn't exist.`);
+		
+		delete this.commands[name];
+	}
+}
+
+module.exports=Handler;
